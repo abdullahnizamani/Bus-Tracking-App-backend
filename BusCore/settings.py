9af09dfd332aa7d5b35249a9cf0ff1816a7bfff3
Abdullah <toonnetwork786@gmail.com>
2026-01-17 17:30:14 +0500

    other modifications, model tweaks.

--- BusCore/settings.py
+++ BusCore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k0=rx!owoep+g94v#ivp(y+^t9pq(sjzacw0z!bv=8$h5xvqlf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
