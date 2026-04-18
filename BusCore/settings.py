5d708e5d473185aeb71b92de89b89ebe2ba9ce7b
Muhammad Abdullah <toonnetwork786@gmail.com>
2026-04-19 00:13:47 +0500

    Fix: removed an extra DEBUG=True that was overriding the env

--- BusCore/settings.py
+++ BusCore/settings.py
@@ -29,7 +29,6 @@ DEBUG = env.bool('DEBUG', default=False)
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
